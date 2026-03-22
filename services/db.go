f3910c0b07e4053a1e24f6db8067f4bc10cc495b
NerdCodex <subburakesh2255@gmail.com>
2026-03-22 20:50:05 +0530

    Final Commit after removing .env

--- services/db.go
+++ services/db.go
@@ -1,49 +1,3 @@
-// package services
-
-// import (
-// 	"fmt"
-// 	"log"
-// 	"os"
-
-// 	"gorm.io/driver/mysql"
-// 	"gorm.io/gorm"
-// 	"gorm.io/gorm/schema"
-// )
-
-// var DB *gorm.DB
-
-// func ConnectToDB() {
-// 	dbusername := os.Getenv("DB_USERNAME")
-// 	dbpassword := os.Getenv("DB_PASSWORD")
-// 	database := os.Getenv("DATABASE")
-// 	dbhost := os.Getenv("DB_HOST")
-// 	dbport := os.Getenv("DB_PORT")
-
-// 	// Validate env vars
-// 	if dbusername == "" || dbpassword == "" || database == "" || dbhost == "" || dbport == "" {
-// 		log.Fatal("DATABASE ENV VARIABLES ARE MISSING")
-// 	}
-
-// 	dsn := fmt.Sprintf(
-// 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-// 		dbusername, dbpassword, dbhost, dbport, database,
-// 	)
-
-// 	var err error
-// 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
-// 		NamingStrategy: schema.NamingStrategy{
-// 			TablePrefix:   "fitnora.",
-// 			SingularTable: true,
-// 		},
-// 	})
-
-// 	if err != nil {
-// 		log.Fatalf("Failed to connect to database: %v", err)
-// 	}
-
-// 	log.Println("Database connection established successfully.")
-// }
-
 package services
 
 import (
@@ -51,7 +5,7 @@ import (
 	"log"
 	"os"
 
-	"gorm.io/driver/postgres"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"gorm.io/gorm/schema"
 )
@@ -65,21 +19,18 @@ func ConnectToDB() {
 	dbhost := os.Getenv("DB_HOST")
 	dbport := os.Getenv("DB_PORT")
 
+	// Validate env vars
 	if dbusername == "" || dbpassword == "" || database == "" || dbhost == "" || dbport == "" {
 		log.Fatal("DATABASE ENV VARIABLES ARE MISSING")
 	}
 
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Kolkata",
-		dbhost,
-		dbusername,
-		dbpassword,
-		database,
-		dbport,
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		dbusername, dbpassword, dbhost, dbport, database,
 	)
 
 	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
 		NamingStrategy: schema.NamingStrategy{
 			TablePrefix:   "fitnora.",
 			SingularTable: true,
@@ -92,3 +43,52 @@ func ConnectToDB() {
 
 	log.Println("Database connection established successfully.")
 }
+
+// package services
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+
+// 	"gorm.io/driver/postgres"
+// 	"gorm.io/gorm"
+// 	"gorm.io/gorm/schema"
+// )
+
+// var DB *gorm.DB
+
+// func ConnectToDB() {
+// 	dbusername := os.Getenv("DB_USERNAME")
+// 	dbpassword := os.Getenv("DB_PASSWORD")
+// 	database := os.Getenv("DATABASE")
+// 	dbhost := os.Getenv("DB_HOST")
+// 	dbport := os.Getenv("DB_PORT")
+
+// 	if dbusername == "" || dbpassword == "" || database == "" || dbhost == "" || dbport == "" {
+// 		log.Fatal("DATABASE ENV VARIABLES ARE MISSING")
+// 	}
+
+// 	dsn := fmt.Sprintf(
+// 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=require TimeZone=Asia/Kolkata",
+// 		dbhost,
+// 		dbusername,
+// 		dbpassword,
+// 		database,
+// 		dbport,
+// 	)
+
+// 	var err error
+// 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{
+// 		NamingStrategy: schema.NamingStrategy{
+// 			TablePrefix:   "fitnora.",
+// 			SingularTable: true,
+// 		},
+// 	})
+
+// 	if err != nil {
+// 		log.Fatalf("Failed to connect to database: %v", err)
+// 	}
+
+// 	log.Println("Database connection established successfully.")
+// }
